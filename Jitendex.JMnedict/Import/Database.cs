e608c11f889dde838ac7db6b7188e1b98e8b7eff
stephenmk <stephenmk@users.noreply.github.com>
2026-02-23 14:51:11 -0600

    Commit transaction after each document import

--- Jitendex.JMnedict/Import/Database.cs
+++ Jitendex.JMnedict/Import/Database.cs
@@ -104,6 +104,8 @@ internal sealed class Database(ILogger<Database> logger, JMnedictContext context
     {
         logger.LogInformation("Updating {Count} entries with data from {Date:yyyy-MM-dd}", diff.SequenceIds.Count, diff.FileHeader.Date);
 
+        using var transaction = context.Database.BeginTransaction();
+
         var aSequences = DtoMapper.LoadSequencesWithoutRevisions(context, diff.SequenceIds);
 
         FileHeaderTable.InsertItem(context, diff.InsertDocument.Header);
@@ -183,5 +185,6 @@ internal sealed class Database(ILogger<Database> logger, JMnedictContext context
         }
 
         RevisionTable.InsertItems(context, revisions);
+        transaction.Commit();
     }
 }
--- Jitendex.JMnedict/Import/Importer.cs
+++ Jitendex.JMnedict/Import/Importer.cs
@@ -47,9 +47,7 @@ internal sealed class Importer
             return;
         }
 
-        using var transaction = context.Database.BeginTransaction();
         await UpdateDatabaseAsync(archiveDirectory, previousDocument);
-        transaction.Commit();
     }
 
     private DateOnly GetPreviousDate() => context.FileHeaders

41154aa6d2199ecde279c71c91d00dba2397c2a3
stephenmk <stephenmk@users.noreply.github.com>
2026-03-29 18:28:12 -0500

    Add `System.Collections.Immutable` to global usings

--- Source/Furigana/Internal/Service.cs
+++ Source/Furigana/Internal/Service.cs
@@ -16,7 +16,6 @@ You should have received a copy of the GNU Affero General Public License along w
 If not, see <https://www.gnu.org/licenses/>.
 */
 
-using System.Collections.Immutable;
 using System.Text;
 using Jitendex.Furigana.Internal.Models;
 
--- Source/Furigana/Solution.cs
+++ Source/Furigana/Solution.cs
@@ -16,8 +16,6 @@ You should have received a copy of the GNU Affero General Public License along w
 If not, see <https://www.gnu.org/licenses/>.
 */
 
-using System.Collections.Immutable;
-
 namespace Jitendex.Furigana;
 
 public sealed record Solution

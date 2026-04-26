00b335dab1452c6460e0370c1938e6122191cf34
stephenmk <stephenmk@users.noreply.github.com>
2026-04-25 20:56:21 -0500

    Set file header template in .editorconfig

--- Source/Import.KanjiVG/Tables/ComponentTable.cs
+++ Source/Import.KanjiVG/Tables/ComponentTable.cs
@@ -1,20 +1,18 @@
-/*
-Copyright (c) 2025-2026 Stephen Kraus
-SPDX-License-Identifier: AGPL-3.0-or-later
-
-This file is part of Jitendex.
-
-Jitendex is free software: you can redistribute it and/or modify it under the terms of
-the GNU Affero General Public License as published by the Free Software Foundation,
-either version 3 of the License or (at your option) any later version.
-
-Jitendex is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-See the GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License along with Jitendex.
-If not, see <https://www.gnu.org/licenses/>.
-*/
+// Copyright (c) Stephen Kraus
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// This file, ComponentTable.cs, is part of Jitendex.
+//
+// Jitendex is free software: you can redistribute it and/or modify it under the terms of
+// the GNU Affero General Public License as published by the Free Software Foundation,
+// either version 3 of the License or (at your option) any later version.
+//
+// Jitendex is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY,
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License along with Jitendex.
+// If not, see <https://www.gnu.org/licenses/>.
 
 using Jitendex.Data;
 using Jitendex.Data.KanjiVG.Entities;
